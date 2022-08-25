ac9077ed7b3e6a9bdffeb83133d522bf44e3f657
Ayan Khan <93464748+samm10@users.noreply.github.com>
2022-08-25 14:27:20 +0530

    Add files via upload

---
+++ myChat/src/Source.java
@@ -0,0 +1,30 @@
+
+import java.awt.Color;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+public class Source {
+
+    public void changeColor(JPanel panel1) {
+        panel1.setBackground(new Color(240, 240, 240));
+
+    }
+
+    public void normalColor(JPanel panel1) {
+        panel1.setBackground(new Color(204, 204, 204));
+
+    }
+
+    public void serverChange(JLabel label1) {
+        label1.setForeground(Color.GREEN);
+    }
+
+    public void serverNormal(JLabel label1) {
+        label1.setForeground(Color.BLACK);
+    }
+
+    public void NormalWhite(JPanel panel1) {
+        panel1.setBackground(new Color(255, 255, 255));
+    }
+
+    
+}
